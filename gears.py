6f45fa9fbfa0efe06256db60550d3e484072a88e
Didar Gill <didargill@outlook.com>
2022-12-04 12:33:34 -0800

    added inputs & cleaned up outputs

--- gears.py
+++ gears.py
@@ -1,43 +1,42 @@
 import math as m
 
-hp = 2.4*1.34
+hp = 1.34*float(input("Input Power (kW): ")) #convert kW to hp
 
-#Geometry
-dp = 4
-Pd = 6 #arb
+#Geometry - chose available gear from McMaster Carr
+dp = float(input("Input Pitch Diameter (in): ")) #4 in
+Pd = float(input("Input Diametral Pitch (in):")) #6 in
 
 Np = dp*Pd
-Ng = Np
+Ng = Np #same size gears for simplicilty & 1:1 VR
 
 gamma_lower = m.atan(Np/Ng)
 gamma_capital = m.atan(Ng/Np)
 
-face_width = 0.86 
+face_width = float(input("Input Face Width: ")) #0.86 in
 d_av = dp - face_width*m.cos(gamma_capital)
 
 #Force Analysis
-rpm = 8000
+rpm = float(input("Input RPM: " )) #8000 motor speed
 T = (hp*5252/rpm)*12
 w_t = 2*T/d_av
 
-pressure_angle = 20
+pressure_angle = 20 #20 degrees
 w_r = w_t*m.tan(pressure_angle*m.pi/180)*m.cos(gamma_lower)
-
 w_a = w_t*m.tan(pressure_angle*m.pi/180)*m.sin(gamma_lower)
 
 #Strength Analysis
-w_t = 2*T/dp
+#w_t = 2*T/dp first w_t equation is more accurate
 
 #Gear Contact Stress
 C_p = 2290 #for steel
-I = 0.065 #Shigley F15-6 p.800
+I = float(input("Input I (F15-6 p.800 Shigley): ")) #0.065 Shigley F15-6 p.800
 K_o = 1.25 #electric motor input = uniform, propeller = fan = light shock Shigley T15-2 p.797 Mott T9-1 p.378
 Q_v = 7 #arb
 B = 0.25*(12-Q_v)**(2/3)
 A = 50+56*(1-B)
 v_t = m.pi*dp*Np/12
 K_v = ((A+m.sqrt(v_t))/B)**B
-K_mb = 1 #both members straddle mounted Shigley p.779
+K_mb = 1 #both members straddle mounted Shigley p.799
 K_m = K_mb+0.0036*face_width**2
 if face_width >= 0.5:
     C_s = 0.5
@@ -67,17 +66,18 @@ HB2 = (sigma_c2 - 23620)/341
 
 #NEED TO FIND MATERIALS AND CALCULATE SF BASED ON THAT
 
-print("dp ",dp)
-print("Pd ",Pd)
-print("Np ",Np)
-print("Gamma Lower: ", gamma_lower)
-print("Gamma Capital ",gamma_capital)
-print("Face Width ", face_width)
-print("Torque ", T)
-print("Tangential Force ", w_t)
-print("Radial Force ", w_r)
-print("Axial Force ", w_a)
-print("Contact Stress", sigma_c)
-print('Hardness - Contact Stress ', HB1)
-print("Allowable Contact Stress", sigma_c2)
-print('Hardness - Allowable Contact Stress ', HB2)
+print("\nPinion Parameters:")
+print("dp ",dp, "in")
+print("Pd ",Pd, "in")
+print("Np ",Np, "in")
+print("Gamma Lower: ", gamma_lower, "rads")
+print("Gamma Capital ",gamma_capital, "rads")
+print("Face Width ", face_width, "in")
+print("Torque ", T, "lb in")
+print("Tangential Force ", w_t, "lb")
+print("Radial Force ", w_r, "lb")
+print("Axial Force ", w_a, "lb")
+print("Contact Stress", sigma_c, "psi")
+print('Hardness - Contact Stress ', HB1, "Brinell")
+print("Allowable Contact Stress", sigma_c2, "psi")
+print('Hardness - Allowable Contact Stress ', HB2, "Brinell")
